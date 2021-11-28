90dc0bfe7f0cd1f5c9138cb6f7839a8582327565
alexandze <alexandze@gmail.com>
2021-11-27 21:53:26 -0500

    Initial Commit

---
+++ Test pratique/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test pratique
+//
+//  Created by Alexandre Andze Kande on 2021-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test pratiqueTests/Test_pratiqueTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_pratiqueTests.swift
+//  Test pratiqueTests
+//
+//  Created by Alexandre Andze Kande on 2021-11-27.
+//
+
+import XCTest
+@testable import Test_pratique
+
+class Test_pratiqueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
